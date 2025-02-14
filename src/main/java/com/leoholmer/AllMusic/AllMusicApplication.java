372e82860d9f9d1fb1a52c9ecc112cae703e1508
Leo Holmer <leonardoholmer1@gmail.com>
2025-02-13 21:42:02 -0300

    first commit

---
+++ src/main/java/com/leoholmer/AllMusic/AllMusicApplication.java
@@ -0,0 +1,13 @@
+package com.leoholmer.AllMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoholmer/AllMusic/AllMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoholmer.AllMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
